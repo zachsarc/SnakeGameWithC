4a3183d4550a2e610175dc68a8d6adca370fbc88
asargis021-blip <asargis021@gmail.com>
2026-04-22 13:34:46 -0400

    Update snakeGame.c

--- snakeGame.c
+++ snakeGame.c
@@ -29,24 +29,59 @@ void init_snake() {
         snake[i].y = start_y;
     }
 }
-void init_direction() { // Set the snake in a random initial direction (JK) ---------Switch case here that combines these 2?!!!!! 
-    int dirs[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    int r = rand() % 4;
-    dir_x = dirs[r][0];
-    dir_y = dirs[r][1];
-}
+void handle_input() { // Primary author: ZL - Check keyboard input and update snake direction
+    int ch = getch();
 
+    if (ch != ERR) {
+        switch (ch) {
+            case KEY_LEFT:
+            case 'a':
+            case 'A':
+                if (dir_x == 1) {
+                    gameover = 1;
+                } else {
+                    dir_x = -1;
+                    dir_y = 0;
+                }
+                break;
 
+            case KEY_RIGHT:
+            case 'd':
+            case 'D':
+                if (dir_x == -1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 1;
+                    dir_y = 0;
+                }
+                break;
 
-void handle_input(){
-    //Check user's input (keys) and update directions (ZL)
-    if (_kbhit()) {
-        switch (tolower(_getch())) {
-            case 'a': if (key != 2) key = 1; break; // Left
-            case 'd': if (key != 1) key = 2; break; // Right
-            case 'w': if (key != 4) key = 3; break; // Up
-            case 's': if (key != 3) key = 4; break; // Down
-            case 'x': gameover = 1; break; // Exit
+            case KEY_UP:
+            case 'w':
+            case 'W':
+                if (dir_y == 1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 0;
+                    dir_y = -1;
+                }
+                break;
+
+            case KEY_DOWN:
+            case 's':
+            case 'S':
+                if (dir_y == -1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 0;
+                    dir_y = 1;
+                }
+                break;
+
+            case 'x':
+            case 'X':
+                gameover = 1;
+                break;
         }
     }
 }
