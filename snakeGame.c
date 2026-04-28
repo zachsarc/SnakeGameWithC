a2e80a87faeafe76389fa28299e0fa74ea0b3277
Jakub Kania <kubakania74@gmail.com>
2026-04-28 00:17:24 +0000

    Game loop added // Smalle fixes - Jake 4/27/26

--- snakeGame.c
+++ snakeGame.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
@@ -16,7 +17,14 @@ Segment snake[MAX_LENGTH];
 int length = 5;
 
 int dir_x, dir_y;
+int gameover = 0;
 
+void init_direction() {//jk
+
+    dir_x = 1;   // start moving right
+    dir_y = 0;//this could also be random 0 or 1, so that it starts in a diff direction else each time 
+
+}
 
 // Initializes the snake in the center of the screen (JK)
 void init_snake() {
@@ -28,23 +36,72 @@ void init_snake() {
         snake[i].y = start_y;
     }
 }
-void init_direction() { // Set the snake in a random initial direction (JK)
-    int dirs[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-    int r = rand() % 4;
-    dir_x = dirs[r][0];
-    dir_y = dirs[r][1];
+void handle_input() { //keyboard input and change snake direction
+    int ch = getch();
+
+    if (ch != ERR) {
+        switch (ch) {
+            case KEY_LEFT:
+            case 'a':
+            case 'A':
+                if (dir_x == 1) {
+                    gameover = 1;
+                } else {
+                    dir_x = -1;
+                    dir_y = 0;
+                }
+                break;
+
+            case KEY_RIGHT:
+            case 'd':
+            case 'D':
+                if (dir_x == -1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 1;
+                    dir_y = 0;
+                }
+                break;
+
+            case KEY_UP:
+            case 'w':
+            case 'W':
+                if (dir_y == 1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 0;
+                    dir_y = -1;
+                }
+                break;
+
+            case KEY_DOWN:
+            case 's':
+            case 'S':
+                if (dir_y == -1) {
+                    gameover = 1;
+                } else {
+                    dir_x = 0;
+                    dir_y = 1;
+                }
+                break;
+
+            case 'x':
+            case 'X':
+                gameover = 1;
+                break;
+        }
+    }
 }
+//------------------------------------------------------------------------
 
 
+void move_snake() { //Move the snake forward and update head position
+    for (int i = length - 1; i > 0; i--) {
+        snake[i] = snake[i - 1];
+    }
 
-void handle_input(){
-    //Check user's input (keys) and update directions
-}
-
-
-
-void move_snake(){
-//move the snake forward, changing the direction by +-1 in y or +-1 in x.
+    snake[0].x += dir_x;
+    snake[0].y += dir_y;
 }
 
 
@@ -60,19 +117,33 @@ refresh();
 
 
 
-void game_loop(){
-//game loop combining the previous functions for movement and input handling 
+void game_loop() {//jk
+    while (!gameover) {
+        handle_input();
+        move_snake();
+        draw();
+        usleep(100000); // control speed
+    }
 }
 
 
 
-int main(){
+int main() {
     srand(time(NULL));
 
+    initscr();            // start ncurses
+    noecho();             // don't print input
+    cbreak();            // disable line buffering
 
-init_snake();
-init_direction();
+    keypad(stdscr, TRUE); // enable arrow keys
+    nodelay(stdscr, TRUE);// non-blocking input
+    curs_set(0);          // hide cursor
 
+    init_snake();
+    init_direction();
 
+    game_loop();
 
+    endwin();             
+    return 0;
 }
